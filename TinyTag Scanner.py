12f1a84fed90e019d0d0f58cd2fb2517988dae0c
Fabrizio Baldini <18417628+Aanok@users.noreply.github.com>
2018-06-26 17:56:14 +0200

    Initial release. Absolute baseline. Rough is an understatement.

---
+++ TinyTag Scanner.py
@@ -0,0 +1,38 @@
+import os.path, Media
+from tinytag import TinyTag
+
+supported_containers = [
+    '.mp3',
+    '.ogg',
+    '.opus',
+    '.mp4',
+    '.m4a',
+    '.flac',
+    '.wma',
+    '.wav'
+]
+
+
+def utf8(string):
+    return string.encode('utf-8') if string else None
+
+
+def Scan(path, files, mediaList, subdirs, language=None, root=None):
+    for f in files:
+        filename, extension = os.path.splitext(f)
+        if extension not in supported_containers:
+            continue
+
+        tags = TinyTag.get(f)
+        print tags
+        plex_track = Media.Track(
+            artist          =   utf8(tags.artist),
+            album           =   utf8(tags.album),
+            title           =   utf8(tags.title),
+            index           =   utf8(tags.track),
+            year            =   utf8(tags.year),
+            disc            =   utf8(tags.disc),
+            album_artist    =   utf8(tags.albumartist)
+        )
+	plex_track.parts.append(f)
+	mediaList.append(plex_track)
