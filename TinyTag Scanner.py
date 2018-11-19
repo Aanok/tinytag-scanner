0d74ddb97b3c93dd23333d49e27de52b3758fee2
Aanok <18417628+Aanok@users.noreply.github.com>
2018-11-19 12:24:27 +0100

    XML-illegal Unicode control characters and inference

--- TinyTag Scanner.py
+++ TinyTag Scanner.py
@@ -1,4 +1,4 @@
-import os.path, Media
+import re, os.path, Media
 from tinytag import TinyTag
 
 supported_containers = [
@@ -13,8 +13,23 @@ supported_containers = [
 ]
 
 
+# Unicode control characters can appear in ID3v2 tags but are not legal in XML.
+RE_UNICODE_CONTROL =  u'([\u0000-\u0008\u000b-\u000c\u000e-\u001f\ufffe-\uffff])' + \
+                      u'|' + \
+                      u'([%s-%s][^%s-%s])|([^%s-%s][%s-%s])|([%s-%s]$)|(^[%s-%s])' % \
+                      (
+                        unichr(0xd800),unichr(0xdbff),unichr(0xdc00),unichr(0xdfff),
+                        unichr(0xd800),unichr(0xdbff),unichr(0xdc00),unichr(0xdfff),
+                        unichr(0xd800),unichr(0xdbff),unichr(0xdc00),unichr(0xdfff)
+                      )
+
+
 def utf8(string):
-    return string.encode('utf-8') if isinstance(string, unicode) else None
+    try:
+        string = re.sub(RE_UNICODE_CONTROL, '', string.strip().encode('utf-8'))
+    except:
+        pass
+    return string
 
 
 def Scan(path, files, mediaList, subdirs, language=None, root=None):
@@ -25,9 +40,9 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
 
         tags = TinyTag.get(f)
         plex_track = Media.Track(
-            artist          =   utf8(tags.artist),
-            album           =   utf8(tags.album),
-            title           =   utf8(tags.title),
+            artist          =   utf8(tags.artist) or '[Unknown Artist]',
+            album           =   utf8(tags.album) or '[Unkown Album]',
+            title           =   utf8(tags.title) or os.path.basename(filename),
             index           =   utf8(tags.track),
             year            =   utf8(tags.year),
             disc            =   utf8(tags.disc),
